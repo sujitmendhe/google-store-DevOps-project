ffcdc54b5722584d2a6a754496d88d43c327a109
sujitmendhe <69568845+sujitmendhe@users.noreply.github.com>
2026-01-08 13:55:51 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ CORS(app)
 
 # ---------------- DATABASE CONFIG ----------------
 db_config = {
-    "host": "micro.cjs80048kd9y.us-west-2.rds.amazonaws.com",
+    "host": "google-store-vpc1.cw9w48kuu6go.us-east-1.rds.amazonaws.com",
     "user": "admin",
     "password": "Cloud123",
     "database": "cloud"
@@ -116,6 +116,7 @@ def get_users():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
     
+
 
 
 
